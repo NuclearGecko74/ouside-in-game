3ba69439e730b2a9465e76c100ef338b1da3f65f
Anton Olguin <antonolguin1@gmail.com>
2025-12-07 14:27:04 -0600

    Update PlayerController

--- Assets/_Game/Scripts/Player/FirstPersonController.cs
+++ Assets/_Game/Scripts/Player/FirstPersonController.cs
@@ -3,111 +3,69 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FirstPersonController : MonoBehaviour
 {
-    [Header("Movement Config")]
-    [SerializeField] private float walkSpeed = 5f;
-    [SerializeField] private float runSpeed = 8f;
-    [SerializeField] private float crouchSpeed = 2.5f;
-    [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float jumpHeight = 1.5f;
+    [Header("Config")]
+    public float walkSpeed = 4.0f;
+    public float gravity = -9.81f;
 
-    [Header("Crouching Config")]
-    [SerializeField] private float crouchHeight = 0.3f;
-    [SerializeField] private float standHeight = 1f;
-    [SerializeField] private float timeToCrouch = 0.25f;
-    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
+    [Header("Refs")]
+    public Camera playerCamera;
+    public float mouseSensitivity = 150f;
 
-    [Header("Camera Config")]
-    [SerializeField] private Camera playerCamera;
-    [SerializeField] private float mouseSensitivity = 2f;
-    [SerializeField] private float lookXLimit = 85f;
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float xRotation = 0f;
 
-    private CharacterController characterController;
-    private Vector3 moveDirection = Vector3.zero;
-    private float rotationX = 0;
-    private bool canMove = true;
-
-    private bool isCrouching = false;
-    private Vector3 targetCenter;
-    private float targetHeight;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-
-        characterController.height = standHeight;
-        characterController.center = new Vector3(0, standHeight / 2, 0);
-
+        controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        transform.Rotate(0, mouseX, 0);
+        HandleMovement();
+        HandleMouseLook();
+    }
 
-        rotationX += -Input.GetAxis("Mouse Y") * mouseSensitivity;
-        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-
-        if (Input.GetKey(crouchKey))
+    void HandleMovement()
+    {
+        if (controller.isGrounded && velocity.y < 0)
         {
-            isCrouching = true;
-            targetHeight = crouchHeight;
-        }
-        else
-        {
-            isCrouching = false;
-            targetHeight = standHeight;
+            velocity.y = -2f;
         }
 
-        float lastHeight = characterController.height;
-        characterController.height = Mathf.Lerp(characterController.height, targetHeight, timeToCrouch * 5 * Time.deltaTime);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 newCenter = characterController.center;
-        newCenter.y = characterController.height / 2f;
-        characterController.center = newCenter;
+        Vector3 move = transform.right * x + transform.forward * z;
 
-        Vector3 camPos = playerCamera.transform.localPosition;
-        camPos.y = characterController.height * 0.9f;
-        playerCamera.transform.localPosition = camPos;
+        controller.Move(move * walkSpeed * Time.deltaTime);
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) && !isCrouching;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 
-        float currentSpeedX = 0;
-        float currentSpeedY = 0;
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        if (canMove)
-        {
-            float inputSpeed = walkSpeed;
-            if (isCrouching) inputSpeed = crouchSpeed;
-            else if (isRunning) inputSpeed = runSpeed;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-            currentSpeedX = inputSpeed * Input.GetAxis("Vertical");
-            currentSpeedY = inputSpeed * Input.GetAxis("Horizontal");
-        }
+        transform.Rotate(Vector3.up * mouseX);
+    }
 
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (transform.forward * currentSpeedX) + (transform.right * currentSpeedY);
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
 
-        if (characterController.isGrounded)
-        {
-            if (Input.GetButton("Jump") && canMove && !isCrouching)
-            {
-                moveDirection.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-            else
-            {
-                moveDirection.y = -1f;
-            }
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY + (gravity * Time.deltaTime);
-        }
+        if (body == null || body.isKinematic) return;
 
-        characterController.Move(moveDirection * Time.deltaTime);
+        if (hit.moveDirection.y < -0.3f) return;
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.linearVelocity = pushDir * 2.0f;
     }
 }
